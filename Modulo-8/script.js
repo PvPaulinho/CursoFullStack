d1996dde51d4016b3d557771eeab105cc0072ca4
Paulo Victor <pvictor36@gmail.com>
2023-02-01 20:14:28 -0300

    Aula 4 - Variaveis

--- Modulo-8/script.js
+++ Modulo-8/script.js
@@ -1,1 +1,18 @@
 console.log("Hello Word");
+
+var a = "Placa de captura";
+
+console.log (a);
+
+var b;
+
+b = "hd";
+
+a = b;
+
+console.log (a);
+console.log (b);
+
+b = "Placa de captura";
+
+console.log (b);
